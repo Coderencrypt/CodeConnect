e2bcfe6bcfe7d1bc0797f2c8e3b788604932ac37
Coderencrypt <160413912+Coderencrypt@users.noreply.github.com>
2026-01-05 00:30:21 +0530

    Fix typo in frontend directory path

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.get("/books",(req,res)=>{
 
 // make our app ready for deployment
 if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../fontend/dist")))
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
     app.get("/{*any}", (req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
