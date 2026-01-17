20a61646f5e68c6bf6cb28c4116272e1110dd105
Coderencrypt <ramlal919293@gmail.com>
2026-01-17 20:55:20 +0530

    server side auth implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -50,10 +50,13 @@ import path from "path";
 import cors from "cors";
 
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express';
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest , functions } from "./lib/inngest.js";
+
+import chatRoutes from "./routes/chatRoutes.js"
 
 const app = express();
 const __dirname = path.resolve();
@@ -62,18 +65,18 @@ const __dirname = path.resolve();
 app.use(express.json());
 // credentials :true meaning ?? ==> server allows a browser to include cookies on request 
 app.use(cors({origin:ENV.CLIENT_URL, credentials : true}));
+app.use(clerkMiddleware());  // this adds auth field to request object : res.auth()
 
 
-app.use("/api/inngest", serve({client : inngest , functions}))
+app.use("/api/inngest", serve({client : inngest , functions}));
+app.use("/api/chat", chatRoutes);
 
 // API Routes
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/books", (req, res) => {
-    res.status(200).json({ msg: "this is a books endpoint" });
-});
+
 
 // Deployment Logic
 if (process.env.NODE_ENV === "production" || ENV.NODE_ENV === "production") {
